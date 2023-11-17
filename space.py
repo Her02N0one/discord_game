5b49b9d0bd9c4263ca0b725970df390287571509
ayd3n <ayd3n1229@gmail.com>
2023-11-17 13:34:08 -0500

    Refactor code for better readability and maintainability

--- space.py
+++ space.py
@@ -1,51 +1,49 @@
+
 class Bubble:
     def __init__(self, id: str):
         self.id = id
-        self.celestial_bodies = {}
+        self.celestial_bodies = dict()
 
-    def attach_celestial_body(self, celestial_body):
+    def attach_celestial_body(self, celestial_body: 'CelestialBody') -> 'CelestialBody':
         if celestial_body.id not in self.celestial_bodies:
             self.celestial_bodies[celestial_body.id] = celestial_body
             celestial_body.bubble = self
-            return self
+            return celestial_body
         else:
             print(f"Celestial Body '{celestial_body.id}' already exists in '{self.id}'")
 
-    def get_hierarchy(self):
+    def find_character(self, character_id: str) -> 'CelestialBody':
+        for celestial_body in self.celestial_bodies.values():
+            if character_id in list(map(celestial_body.characters, lambda character: character.id)):
+                return celestial_body
+
+    def get_hierarchy(self) -> str:
         hierarchy = f"Bubble: {self.id}\n"
         for celestial_body in self.celestial_bodies.values():
             hierarchy += celestial_body.get_hierarchy(indent_level=1)
         return hierarchy
 
-
-class HomePlanetBubble(Bubble):
-    pass  # Specific methods or properties for the home planet
-
-
-class IncipisphereBubble(Bubble):
-    pass  # Specific methods or properties for the Incipisphere
-
-
 class CelestialBody:
-    def __init__(self, id: str, description: str, bubble=None):
+    def __init__(self, id: str, description: str):
         self.id = id
         self.description = description
-        self.locations = {}  # Locations on the celestial body
-        self.bubble = bubble.attach_celestial_body(self) if bubble else None
+        self.locations = dict()
+        self.bubble = None
 
-    def attach_location(self, location):
+    def attach_location(self, location: 'Location') -> 'Location':
         if location.id not in self.locations:
             self.locations[location.id] = location
             location.celestial_body = self
-            return self
+            return location
         else:
             print(f"Location '{location.id}' already exists in '{self.id}'")
 
-    def attach_to_bubble(self, bubble):
-        bubble.attach_celestial_body(self)
-        return self
+    def find_character(self, character_id: str) -> 'Location':
+        for location in self.locations.values():
+            if character_id in list(map(location.characters, lambda character: character.id)):
+                return location
 
-    def get_hierarchy(self, indent_level=0):
+    def get_hierarchy(self, indent_level: int = 0) -> str:
         indent = "  " * indent_level
         hierarchy = f"{indent}Celestial Body: {self.id} - {self.description}\n"
         for location in self.locations.values():
@@ -54,104 +52,85 @@ class CelestialBody:
 
 
 class Location:
-    def __init__(self, id: str, description: str, celestial_body=None):
+    def __init__(self, id: str, description: str):
         self.id = id
         self.description = description
-        self.rooms = {}
-        self.celestial_body = celestial_body.attach_location(self) if celestial_body else None
-    
-    def attach_room(self, room):
-        if room.id not in self.rooms:
-            self.rooms[room.id] = room
-            room.set_location(self)
-            return self
+        self.rooms = []
+        self.celestial_body = None
+
+    def attach_room(self, room: 'Room') -> 'Room':
+        if room not in self.rooms:
+            self.rooms.append(room)
+            room.location = self
+            return room
         else:
             print(f"Room '{room.id}' already exists in '{self.id}'")
 
-    def attach_to_celestial_body(self, celestial_body):
-        celestial_body.attach_location(self)
-        return self
+    def find_character(self, character_id: str) -> 'Room':
+        for room in self.rooms:
+            if character_id in list(map(room.characters, lambda character: character.id)):
+                return room
+            
+    def get_path(self, from_room, to_room) -> list:
+        if (from_room in self.rooms) and (to_room in self.rooms):
+            # TODO: Implement pathfinding algorithm
+            return [from_room, to_room]
 
-    def get_hierarchy(self, indent_level=0):
+    def get_hierarchy(self, indent_level: int = 0) -> str:
         indent = "  " * indent_level
         hierarchy = f"{indent}Location: {self.id} - {self.description}\n"
-        for room in self.rooms.values():
+        for room in self.rooms:
             hierarchy += room.get_hierarchy(indent_level + 1)
         return hierarchy
 
-# class House(Location):
-#     def __init__(self, id: str, description: str):
-#         super().__init__(id, description)
-#         self.rooms = {}
-        
-#         self.attach_room(Room("Living Room", "A room for living"))
-
 
 class Room:
-    def __init__(self, id: str, description: str, location=None):
+    def __init__(self, id: str, description: str):
         self.id = id
         self.description = description
-        self.characters = []  # Characters in the room
-        self.objects = []  # Objects in the room
-        self.location = location.attach_room(self) if location else None
+        self.players = list()
+        self.objects = list()
+        self.connected_rooms = dict()
+        self.location = None
 
-        self.connected_rooms = {}  # Adjacency list for connected rooms, the string id can potentially contain metadata about the connection. For example, "hallway|locked|key:1234" could indicate a locked door that can be unlocked with the key 1234
-    
-    def set_location(self, location):
-        self.location = location.attach_room(self)
-        return self
-
-    def connect_room(self, room, bidirectional=True, attached=True, *args, **kwargs):
-        if room not in self.connected_rooms.values():
-            connection = f"{room.id}" + "".join([f"|{arg}" for arg in args]) + "".join([f"|{key}:{value}" for key, value in kwargs.items()])
+    def connect_room(self, room: 'Room', bidirectional: bool = True, attached: bool = True, *args, **kwargs) -> None:
+        if room not in self.connected_rooms:
+            connection = f"{room.id}" # + "".join([f"|{arg}" for arg in args]) + "".join([f"|{key}:{value}" for key, value in kwargs.items()])
             self.connected_rooms[connection] = room
-
+            print(self.connected_rooms)
             if bidirectional:
-                room.connect_room(self, bidirectional=False, attached=False)
+                room.connect_room(self, bidirectional=False, attached=False, *args, **kwargs)
             if attached:
-                self.location.attach_room(room)  # Attach the room to the same location as the current room
-
-                
-            return room
+                self.location.attach_room(room)
+            
+            return connection
         else:
             print(f"Room '{room.id}' already connected to '{self.id}'")
 
-    
-    def disconnect(self):
-        for connection, connected_room in list(self.connected_rooms.items()):
-            self.disconnect_room(connected_room)
-            connected_room.disconnect_room(self)
-        return self
+    def get_hierarchy(self, indent_level: int = 0) -> str:
+        return "  " * indent_level + f"Room: {self.id} - {self.description}\n"
 
-    def disconnect_room(self, room):
-        for connection, connected_room in list(self.connected_rooms.items()):
-            if connected_room == room:
-                del self.connected_rooms[connection]
-        return self
-
-    def add_character(self, character):
-        self.characters.append(character)
-        return self
-
-    def add_object(self, item):
-        self.objects.append(item)
-        return self
-
-    def get_hierarchy(self, indent_level=0):
-        indent = "  " * indent_level
-        return f"{indent}Room: {self.id} - {self.description}\n"
 
 if __name__ == '__main__':
     universe = Bubble("universe_a")
-    earth = CelestialBody("earth", "The planet we live on", universe)
-    johns_house = Location("john_house", "The house John lives in", earth)
+    earth = universe.attach_celestial_body(CelestialBody("earth", "The planet we live on"))
+    johns_house = earth.attach_location(Location("johns_house", "The house John lives in"))
+
+    bedroom = Room("bedroom", "The room John sleeps in")
+    hallway = Room("hallway", "The hallway outside John's bedroom")
+    study = Room("study", "The study")
+    balcony = Room("balcony", "The balcony")
 
 
-    bedroom = Room("bedroom", "The room John sleeps in", johns_house)
+    johns_house.attach_room(bedroom)
+    johns_house.attach_room(hallway)
+    johns_house.attach_room(study)
+    johns_house.attach_room(balcony)
 
-    hallway = Room("hallway", "The hallway outside John's bedroom", johns_house)
     bedroom.connect_room(hallway)
+    hallway.connect_room(study)
+    hallway.connect_room(balcony)
 
 
+    print(johns_house.get_path(bedroom, balcony))
     print(universe.get_hierarchy())
-    
